d5a8da05d3e99a6ba081ac8536b764fa9254408f
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2025-01-12 01:22:06 +0000

    update

--- image_segmentation/main.py
+++ image_segmentation/main.py
@@ -1,9 +1,13 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11n-seg.pt")
+
+# try to predict
 results = model(
-    "./data/test/m2c_909_jpg.rf.20d754056ce1f9c2510f80dab1504bcb.jpg",
+    "./data/yolo/test/images/m2c_909_jpg.rf.20d754056ce1f9c2510f80dab1504bcb.jpg",
     save=True,
-    show_boxes=False,
+    show_boxes=True,
     exist_ok=True,
 )
+
+results = model.train(data="./golf_train.yaml", epochs=10, imgsz=640)
